c291c6bacf7dfe2a1cc0c9ce2e93ad77ccb44912
Andrea Criollo <113551844+acriollo1@users.noreply.github.com>
2023-02-22 20:59:08 +0000

    author, price, title, rank

--- main.py
+++ main.py
@@ -15,7 +15,30 @@ headers = {
 page = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(page.content, 'html.parser')
+
+books = soup.find_all(id="gridItemRoot")
+
+book = books[0]
+
+rank = book.find('span', class_='zg-bdg-text').text[1:]
+
+children = book.find('div', class_="zg-grid-general-faceout").div
+children.contents[0]
+
+#title = book.find('div', class_="_cDEzb_p13n-sc-css-line-clamp-1_1Fn1y")
+title = children.contents[1].text
+author = children.contents[2].text
+price = children.contents[-1].text
+
+print(price)
+print(author)
+print(title)
+print(rank)
+
+# print(books[0].prettify())
+
 # print(soup.prettify())
+
 #get all books
 
 #get info from first book
